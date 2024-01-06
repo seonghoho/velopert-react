d7aeed5ba63938775e113ca75b21636f388a884a
최성호 <chltjdgh3@naver.com>
2024-01-06 11:15:21 +0900

    [240105] React 연습 프로젝트 생성

---
+++ start/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h2>
+          hi
+      </h2>
+      </div>
+  );
+}
+
+export default App;
